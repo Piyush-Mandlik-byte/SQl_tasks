4487d8213e0895a5604570a67466d27962fb754f
Piyush-Mandlik-byte <mandlikshailesh123@gmail.com>
2025-09-10 18:12:45 +0530

    Add files via upload

---
+++ moviedata.sql
@@ -0,0 +1,609 @@
+create table moviedata(
+id serial primary key,
+name varchar,
+premiere date
+)
+
+select * from moviedata
+
+insert into moviedata(id, name, premiere)
+values (00001,'movie1','1-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00002,'movie2','2-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00003,'movie3','3-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00004,'movie4','4-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00005,'movie5','5-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00006,'movie6','6-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00007,'movie7','7-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00008,'movie8','8-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00009,'movie9','9-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00010,'movie10','10-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00011,'movie11','11-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00012,'movie12','12-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00013,'movie13','13-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00014,'movie14','14-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00015,'movie15','15-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00016,'movie16','16-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00017,'movie17','17-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00018,'movie18','18-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00019,'movie19','19-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00020,'movie20','20-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00021,'movie21','21-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00022,'movie22','22-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00023,'movie23','23-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00024,'movie24','24-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00025,'movie25','25-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00026,'movie26','26-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00027,'movie27','27-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00028,'movie28','28-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00029,'movie29','29-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00030,'movie30','30-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00031,'movie31','31-1-2000')
+
+insert into moviedata(id, name, premiere)
+values (00032,'movie32','1-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00033,'movie33','2-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00034,'movie34','3-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00035,'movie35','4-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00036,'movie36','5-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00037,'movie37','6-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00038,'movie38','7-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00039,'movie39','8-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00040,'movie40','9-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00041,'movie41','10-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00042,'movie42','11-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00043,'movie43','12-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00044,'movie44','13-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00045,'movie45','14-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00046,'movie46','15-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00047,'movie47','16-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00048,'movie48','17-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00049,'movie49','18-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00050,'movie50','19-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00051,'movie51','20-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00052,'movie52','21-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00053,'movie53','22-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00054,'movie54','23-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00055,'movie55','24-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00056,'movie56','25-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00057,'movie57','26-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00058,'movie58','27-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00059,'movie59','28-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00060,'movie60','29-2-2000')
+
+insert into moviedata(id, name, premiere)
+values (00061,'movie61','1-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00062,'movie62','2-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00063,'movie63','3-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00064,'movie64','4-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00065,'movie65','5-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00066,'movie66','6-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00067,'movie67','7-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00068,'movie68','8-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00069,'movie69','9-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00070,'movie70','10-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00071,'movie71','11-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00072,'movie72','12-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00073,'movie73','13-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00074,'movie74','14-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00075,'movie75','15-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00076,'movie76','16-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00077,'movie77','17-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00078,'movie78','18-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00079,'movie79','19-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00080,'movie80','20-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00081,'movie81','21-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00082,'movie82','22-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00083,'movie83','23-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00084,'movie84','24-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00085,'movie85','25-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00086,'movie86','26-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00087,'movie87','27-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00088,'movie88','28-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00089,'movie89','29-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00090,'movie90','30-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00091,'movie91','31-3-2000')
+
+insert into moviedata(id, name, premiere)
+values (00092,'movie92','1-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00093,'movie93','2-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00094,'movie94','3-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00095,'movie95','4-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00096,'movie96','5-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00097,'movie97','6-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00098,'movie98','7-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00099,'movie99','8-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00100,'movie100','9-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00101,'movie101','10-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00102,'movie102','11-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00103,'movie103','12-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00104,'movie104','13-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00105,'movie105','14-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00106,'movie106','15-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00107,'movie107','16-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00108,'movie108','17-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00109,'movie109','18-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00110,'movie110','19-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00111,'movie111','20-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00112,'movie112','21-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00113,'movie113','22-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00114,'movie114','23-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00115,'movie115','24-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00116,'movie116','25-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00117,'movie117','26-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00118,'movie118','17-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00119,'movie119','18-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00120,'movie120','19-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00121,'movie121','20-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00122,'movie122','21-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00123,'movie123','22-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00124,'movie124','23-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00125,'movie125','24-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00126,'movie126','25-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00127,'movie127','26-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00128,'movie128','27-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00129,'movie129','28-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00130,'movie130','29-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00131,'movie131','30-4-2000')
+
+insert into moviedata(id, name, premiere)
+values (00132,'movie132','1-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00133,'movie133','2-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00134,'movie134','3-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00135,'movie135','4-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00136,'movie136','5-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00137,'movie137','6-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00138,'movie138','7-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00139,'movie139','8-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00140,'movie140','9-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00141,'movie141','10-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00142,'movie142','11-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00143,'movie143','12-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00144,'movie144','13-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00145,'movie145','14-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00146,'movie146','15-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00147,'movie147','16-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00148,'movie148','17-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00149,'movie149','18-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00150,'movie150','19-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00151,'movie151','20-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00152,'movie152','21-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00153,'movie153','22-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00154,'movie154','23-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00155,'movie155','24-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00156,'movie156','25-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00157,'movie157','26-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00158,'movie158','27-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00159,'movie159','28-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00160,'movie160','29-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00161,'movie161','30-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00162,'movie162','31-5-2000')
+
+insert into moviedata(id, name, premiere)
+values (00163,'movie163','1-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00164,'movie164','2-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00165,'movie165','3-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00166,'movie166','4-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00167,'movie167','5-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00168,'movie168','6-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00169,'movie169','7-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00170,'movie170','8-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00171,'movie171','9-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00172,'movie172','10-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00173,'movie173','11-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00174,'movie174','12-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00175,'movie175','13-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00176,'movie176','14-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00177,'movie177','15-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00178,'movie178','16-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00179,'movie179','17-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00180,'movie180','18-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00181,'movie181','19-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00182,'movie182','20-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00183,'movie183','21-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00184,'movie184','22-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00185,'movie185','23-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00186,'movie186','24-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00187,'movie187','25-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00188,'movie188','26-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00189,'movie189','27-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00190,'movie190','28-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00191,'movie191','29-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00192,'movie192','30-6-2000')
+
+insert into moviedata(id, name, premiere)
+values (00193,'movie193','1-7-2000')
+
+insert into moviedata(id, name, premiere)
+values (00194,'movie194','2-7-2000')
+
+insert into moviedata(id, name, premiere)
+values (00195,'movie195','3-7-2000')
+
+insert into moviedata(id, name, premiere)
+values (00196,'movie196','4-7-2000')
+
+insert into moviedata(id, name, premiere)
+values (00197,'movie197','5-7-2000')
+
+insert into moviedata(id, name, premiere)
+values (00198,'movie198','6-7-2000')
+
+insert into moviedata(id, name, premiere)
+values (00199,'movie199','7-7-2000')
+
+insert into moviedata(id, name, premiere)
+values (00200,'movie200','8-7-2000')
+
+select * from moviedata
